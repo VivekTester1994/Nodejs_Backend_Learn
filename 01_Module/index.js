911812dded9fff10875542cc2c7cd1266f06c49b
Vivekjacob <Vivekjacob1994@hotmail.com>
2026-07-30 13:19:47 +0800

    FirstCommit

---
+++ 01_Module/index.js
@@ -0,0 +1,9 @@
+//Reading File
+// const fs = require("fs");
+// const data = fs.readFileSync("notes.txt", "utf-8");
+// console.log(data);
+//Writing a File
+const fs = require("fs");
+fs.writeFileSync("notes.txt", "utf-8")
+const data={name :'John',age:30,city:'NewYork'}
+console.log(data);
