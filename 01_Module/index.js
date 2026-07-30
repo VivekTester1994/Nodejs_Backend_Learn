8af72f070c068e789186a3decf97034dae7ea0fe
Vivekjacob <Vivekjacob1994@hotmail.com>
2026-07-30 15:37:27 +0800

    SecondCommit

--- 01_Module/index.js
+++ 01_Module/index.js
@@ -4,6 +4,6 @@
 // console.log(data);
 //Writing a File
 const fs = require("fs");
-fs.writeFileSync("notes.txt", "utf-8")
-const data={name :'John',age:30,city:'NewYork'}
+fs.writeFileSync("notes.txt", "utf-8");
+const data = { name: "John", age: 30, city: "NewYork" };
 console.log(data);
